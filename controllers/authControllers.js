03fafd04d6fab1215598324483d79634ad2ca806
eltonwcm00 <eltonwcm00@gmail.com>
2021-06-07 18:50:08 +0800

    mongoose validation

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,43 @@
 const User = require('../models/User');
 
+// Custom error handler
+const handleErrors = (err) => {
+    
+     /* Validation errors 
+       when it matches sentence 'user validation failed
+       message */
+       
+    /* Taken from the err.message in the format of :
+       {
+           validator: [Function],
+           message: '',
+           type: '',
+           path: '',
+           value: ''
+       }
+    */
+    
+    // Handle errors
+    console.log(err.message, err.code);
+    let errors = { email: '', password: ''}; /* '', empty at the beginning to assign error messages */
+
+    // Duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'That email is already registered';
+        return errors;
+    }
+
+    // Validate errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(( {properties} ) => {
+            // console.log(error.properties);
+            errors[properties.path] = properties.message;
+        })
+    }
+
+    return errors;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -19,8 +57,9 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user); /* Send to the db as in a json format */
     
     } catch (err) {
-        console.log(err);
-        res.status(400).send('Error, user is not created'); 
+        const errors = handleErrors(err);
+        res.status(400).json( {errors} );
+        // res.status(400).send('Error, user is not created'); 
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema ({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'], // Custom error message
         unique: true,  /* No 2nd sign up */
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
     
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
     }
 });
 
